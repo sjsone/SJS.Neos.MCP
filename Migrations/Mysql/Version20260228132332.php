6613110d25a7c7600d4893b33e6e69e7900d32bc
sjsone <simon@sjs.one>
2026-03-03 12:53:59 +0100

    chore: removed hard database requirement for migrations

--- Migrations/Mysql/Version20260228132332.php
+++ Migrations/Mysql/Version20260228132332.php
@@ -19,24 +19,12 @@ final class Version20260228132332 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1010Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1010Platform'."
-        );
-
         $this->addSql('CREATE TABLE sjs_neos_mcp_domain_model_agent (persistence_object_identifier VARCHAR(40) NOT NULL, party VARCHAR(40) DEFAULT NULL, name VARCHAR(255) NOT NULL, createdat DATETIME NOT NULL, token VARCHAR(255) NOT NULL, INDEX IDX_58354A3189954EE0 (party), PRIMARY KEY(persistence_object_identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent ADD CONSTRAINT FK_58354A3189954EE0 FOREIGN KEY (party) REFERENCES neos_party_domain_model_abstractparty (persistence_object_identifier)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1010Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1010Platform'."
-        );
-
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent DROP FOREIGN KEY FK_58354A3189954EE0');
         $this->addSql('DROP TABLE sjs_neos_mcp_domain_model_agent');
     }
--- Migrations/Mysql/Version20260301204324.php
+++ Migrations/Mysql/Version20260301204324.php
@@ -19,12 +19,6 @@ final class Version20260301204324 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1010Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1010Platform'."
-        );
-
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent ADD account VARCHAR(40) DEFAULT NULL, ADD onlyallowedroleidentifiers LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:simple_array)\'');
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent ADD CONSTRAINT FK_58354A317D3656A4 FOREIGN KEY (account) REFERENCES neos_flow_security_account (persistence_object_identifier)');
         $this->addSql('CREATE INDEX IDX_58354A317D3656A4 ON sjs_neos_mcp_domain_model_agent (account)');
@@ -32,12 +26,6 @@ final class Version20260301204324 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1010Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1010Platform'."
-        );
-
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent DROP FOREIGN KEY FK_58354A317D3656A4');
         $this->addSql('DROP INDEX IDX_58354A317D3656A4 ON sjs_neos_mcp_domain_model_agent');
         $this->addSql('ALTER TABLE sjs_neos_mcp_domain_model_agent DROP account, DROP onlyallowedroleidentifiers');
